3ba59bce1057ee52da4a6a43599a3926799008d0
ert11er <169079397+ert11er@users.noreply.github.com>
2026-05-10 16:01:19 +0300

    eryu

--- bids_sorter.py
+++ bids_sorter.py
@@ -178,7 +178,7 @@ def sanitize_app(app):
             app["iconURL"] = "https://placehold.co/512x512"  # default if missing
     if not "name" in app:
         app.clear()
-    if "name" in app == "":
+    if app[name] == "":
         app.clear()
     
 
